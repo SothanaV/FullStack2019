624dd16cdd9f47a205120746b3de86dfd4e3456c
SothanaV <nai6320@gmail.com>
2019-06-04 19:47:53 +0700

    add all

--- 02MyBakery/MyBakery/pos/urls.py
+++ 02MyBakery/MyBakery/pos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import list_item, add_item
+from .views import list_item, add_item, edit_item, sell_item
 
 urlpatterns = [
     path('list' ,list_item, name='list_item'),
     path('add', add_item, name='add_item'),
+    path('edit/<int:pk>', edit_item, name='edit_item'),
+    path('sell/<int:pk>', sell_item, name='sell_item'),
 ]
--- 02MyBakery/MyBakery/pos/views.py
+++ 02MyBakery/MyBakery/pos/views.py
@@ -1,13 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Category, Item, Trasaction
 from .forms import ItemForm
 # Create your views here.
 def list_item(request):
     item = Item.objects.all().order_by('category')
     context = {'item':item}
-    return render(request,'pos/listitem.htm',context)
+    return render(request,'pos/listitem.html',context)
 
 def add_item(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+                form.save()
     form = ItemForm()
     context = {'form':form}
-    return render(request,'pos/edit.htm',context)
+    return render(request,'pos/add.html',context)
+
+def edit_item(request,pk):
+    if request.method == 'POST':
+        item = Item.objects.get(pk=pk)
+        form = ItemForm(request.POST, request.FILES,instance=item)
+        if form.is_valid():
+                form.save()
+        return redirect('list_item')
+    form = ItemForm(instance=Item.objects.get(pk=pk))
+    context = {'form':form}
+    return render(request,'pos/edit.html',context)
+
+def sell_item(request,pk):
+    if request.method == 'POST':
+        try:
+            item = Item.objects.get(pk=pk)
+            # item.in_stock = item.in_stock-int(request.POST['amount'])
+            amount = int(request.POST['amount'])
+            item.in_stock -= amount
+            item.save()
+            Trasaction.objects.create(
+                    item=item,
+                    amount = amount,
+                    total_price = item.price*amount
+                    )
+        except Exception as e:
+            print(e)
+            raise e
+        else:
+            return redirect('list_item')
+    else:
+        item = Item.objects.get(pk=pk)
+        context = {'item':item}
+        return render(request,'pos/sell.html',context)
